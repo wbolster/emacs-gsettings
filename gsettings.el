8e81996a545365e390f70bca4cb5050f8d4cf17f
wouter bolsterlee <wouter@bolsterl.ee>
2019-04-28 18:00:58 +0200

    add package skeleton

---
+++ gsettings.el
@@ -0,0 +1,21 @@
+;;; gsettings.el --- GSettings (Gnome) helpers -*- lexical-binding: t; -*-
+
+;; Author: wouter bolsterlee <wouter@bolsterl.ee>
+;; Keywords: languages
+;; URL: https://github.com/wbolster/emacs-gsettings
+;; Package-Requires: ((emacs "24") (gvariant "0.1.0"))
+;; Version: 0.1.0
+
+;; Copyright 2019 wouter bolsterlee. Licensed under the 3-Clause BSD License.
+
+;;; Commentary:
+
+;; This package provides helpers for Gnome GSettings.
+
+;;; Code:
+
+(require 'gvariant)
+
+
+(provide 'gsettings)
+;;; gsettings.el ends here
